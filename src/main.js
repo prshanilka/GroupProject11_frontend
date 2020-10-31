a7eb0781c4a674c708e26c96f5fd912eba68c5e8
unknown <sahanbcsrh@gmail.com>
2020-10-31 10:06:44 +0530

    elder form creating start

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
 
 // BootstrapVue add
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from "bootstrap-vue";
 // Router & Store add
-import router from './router'
-import store from './store'
+import router from "./router";
+import store from "./store";
 // Multi Language Add
-import en from './locales/en.json'
-import es from './locales/es.json'
-import VueI18n from 'vue-i18n'
-import { firebaseConfig } from './constants/config'
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+import VueI18n from "vue-i18n";
+import { firebaseConfig } from "./constants/config";
 // Notification Component Add
-import Notifications from './components/Common/Notification'
+import Notifications from "./components/Common/Notification";
 // Breadcrumb Component Add
-import Breadcrumb from './components/Common/Breadcrumb'
+import Breadcrumb from "./components/Common/Breadcrumb";
 // RefreshButton Component Add
-import RefreshButton from './components/Common/RefreshButton'
+import RefreshButton from "./components/Common/RefreshButton";
 // Colxx Component Add
-import Colxx from './components/Common/Colxx'
+import Colxx from "./components/Common/Colxx";
 // Perfect Scrollbar Add
-import vuePerfectScrollbar from 'vue-perfect-scrollbar'
-import contentmenu from 'v-contextmenu'
-import VueLineClamp from 'vue-line-clamp'
-import VueScrollTo from 'vue-scrollto'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import { getCurrentLanguage } from './utils'
+import vuePerfectScrollbar from "vue-perfect-scrollbar";
+import contentmenu from "v-contextmenu";
+import VueLineClamp from "vue-line-clamp";
+import VueScrollTo from "vue-scrollto";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { getCurrentLanguage } from "./utils";
 
 Vue.use(BootstrapVue);
 Vue.use(VueI18n);
@@ -34,28 +34,28 @@ const messages = { en: en, es: es };
 const locale = getCurrentLanguage();
 const i18n = new VueI18n({
   locale: locale,
-  fallbackLocale: 'en',
+  fallbackLocale: "en",
   messages
 });
 Vue.use(Notifications);
-Vue.use(require('vue-shortkey'));
+Vue.use(require("vue-shortkey"));
 Vue.use(contentmenu);
 Vue.use(VueScrollTo);
 Vue.use(VueLineClamp, {
   importCss: true
 });
 
-Vue.component('piaf-breadcrumb', Breadcrumb);
-Vue.component('b-refresh-button', RefreshButton);
-Vue.component('b-colxx', Colxx);
-Vue.component('vue-perfect-scrollbar', vuePerfectScrollbar);
+Vue.component("piaf-breadcrumb", Breadcrumb);
+Vue.component("b-refresh-button", RefreshButton);
+Vue.component("b-colxx", Colxx);
+Vue.component("vue-perfect-scrollbar", vuePerfectScrollbar);
 
 firebase.initializeApp(firebaseConfig);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 export default new Vue({
   i18n,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
