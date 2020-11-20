ac6c70fc82e4d4ba5241e14d1cb02a88aef8a80d
unknown <gmgpathum@gmail.com>
2020-11-20 20:59:45 +0530

    nnn

--- src/views/elde-allowance-view/divisional secretary/list_of_grama_divisions.vue
+++ src/views/elde-allowance-view/divisional secretary/list_of_grama_divisions.vue
@@ -4,33 +4,37 @@
 		<b-row>
     		<b-colxx xxs="12">
       
-     		<b-row class="icon-cards-row mb-3">
-        	<b-colxx xxs="6" sm="4" md="3" lg="2">
-    		<icon-card title="Gampaha North" icon="iconsminds-home-1" value="222/B" />
-        	</b-colxx>
-        	<b-colxx xxs="6" sm="4" md="3" lg="2">
-          	<icon-card
-            	:title="$t('dashboards.completed-orders')"
-            	icon="iconsminds-home-1"
-            	:value="32"
-          	/>
-        </b-colxx>
-        <b-colxx xxs="6" sm="4" md="3" lg="2">
-          <icon-card
-            :title="$t('dashboards.refund-requests')"
-            icon="iconsminds-home-1"
-            :value="74"
-          />
-        </b-colxx>
-        <b-colxx xxs="6" sm="4" md="3" lg="2">
-          <icon-card
-            :title="$t('dashboards.new-comments')"
-            icon="iconsminds-home-1"
-            :value="25"
-          />
-        </b-colxx>
-      </b-row>
-    	</b-colxx>
+     			<b-row class="icon-cards-row mb-3">
+
+        		<b-colxx xxs="6" sm="4" md="3" lg="2">
+    				<icon-card title="Gampaha North" icon="iconsminds-home-1" value="222/B" />
+        		</b-colxx>
+
+        		<b-colxx xxs="6" sm="4" md="3" lg="2">
+          			<icon-card
+            		:title="$t('dashboards.completed-orders')"
+            		icon="iconsminds-home-1"
+            		:value="32"
+          			/>
+        		</b-colxx>
+
+        		<b-colxx xxs="6" sm="4" md="3" lg="2">
+          			<icon-card
+            		:title="$t('dashboards.refund-requests')"
+            		icon="iconsminds-home-1"
+            		:value="74"
+          			/>
+        		</b-colxx>
+
+        		<b-colxx xxs="6" sm="4" md="3" lg="2">
+          			<icon-card
+            		:title="$t('dashboards.new-comments')"
+            		icon="iconsminds-home-1"
+            		:value="25"
+         			/>
+        		</b-colxx>
+      			</b-row>
+    		</b-colxx>
   		</b-row>
 	</AppLayout>
 </template>
