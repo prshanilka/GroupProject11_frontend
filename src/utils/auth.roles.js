518ccf7b020a4579367e3889b22d1b3967ebc964
unknown <sahanbcsrh@gmail.com>
2020-11-28 14:31:42 +0530

    menu Update

--- src/utils/auth.roles.js
+++ src/utils/auth.roles.js
@@ -2,7 +2,9 @@ export const UserRole = {
   Admin: 0,
   Elders: 10,
   DivisionalOfficers: 20,
+  DivisionalOffOfficer: 24,
+  DivisionalOffHead: 26,
   PostOfficers: 30,
   GramaNiladariOffices: 40,
-  Editor: 1
+  Editor: 100
 };
