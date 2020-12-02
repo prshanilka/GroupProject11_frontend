dbc5e283ac4dda4edd84fe5c0e966609f2dcadb6
unknown <sahanbcsrh@gmail.com>
2020-12-03 02:24:18 +0530

    modify

--- src/views/elde-allowance-view/divisional_secretary/view_elder_application_verify.vue
+++ src/views/elde-allowance-view/divisional_secretary/view_elder_application_verify.vue
@@ -1,134 +1,129 @@
 <template>
-	<AppLayout>
-		<b-colxx xl="10" lg="12" style="margin:auto ">
+	<b-colxx xl="10" lg="12" style="margin:auto ">
+		<b-colxx lg="12" md="12" class="m-lg-4 text-center" style="mrgin-top:50px;">
+			<h1>Elder Details for Verification</h1>
+		</b-colxx>
 
-			<b-colxx lg="12" md="12" class="m-lg-4 text-center" style="mrgin-top:50px;">
-				<h1>Elder Details for Verification</h1>
-			</b-colxx>
+		<b-card class="mb-4 text-center" v-show="!submit_div">
+			<h2 class="mb-2">You SuccecFully Verified The Payment Details</h2>
+			<p>Verified Details are Submitted Now</p>
+		</b-card>
 
-			<b-card class="mb-4 text-center" v-show="!submit_div">
-					<h2 class="mb-2">You SuccecFully Verified The Payment Details</h2>
-					<p>Verified Details are Submitted Now</p>
-			</b-card>
+		<b-card no-body class="mb-4" v-show="submit_div">
+			<b-row>
+				<b-colxx lg="6" md="12" class="mb-4">
+					<div class="position-absolute card-top-buttons"></div>
+					<single-lightbox
+						thumb="/assets/img/profiles/1.jpg"
+						large="/assets/img/profiles/1.jpg"
+						class-name="card-img-top "
+						class="m-4"
+					/>
+				</b-colxx>
+				<b-colxx lg="6" md="12" class="mb-4">
+					<div class="m-4">
+						<p class="text-muted text-small mb-2">Elder Full Name</p>
+						<p class="mb-3">{{elder.name}}</p>
 
-			<b-card no-body class="mb-4" v-show="submit_div">
+						<p class="text-muted text-small mb-2">Elder Address</p>
+						<p class="mb-3">{{elder.address}}</p>
+
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Sex:</span>
+							{{elder.sex}}
+						</p>
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Phone No:</span>
+							{{elder.number}}
+						</p>
+
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Nic No:</span>
+							{{elder.nic_id}}
+						</p>
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Email:</span>
+							{{elder.email}}
+						</p>
+
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Date of Birth:</span>
+							{{elder.birth_day}}
+						</p>
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Age:</span>
+							{{elder.birth_day}} ---> 97
+						</p>
+					</div>
+				</b-colxx>
+			</b-row>
+			<b-card-body>
 				<b-row>
-					
 					<b-colxx lg="6" md="12" class="mb-4">
-						<div class="position-absolute card-top-buttons"></div>
-						<single-lightbox
-							thumb="/assets/img/profiles/1.jpg"
-							large="/assets/img/profiles/1.jpg"
-							class-name="card-img-top "
-							class="m-4"
-						/>
+						<p class="mb-3">
+							<span class="text-muted text-small mb-2">Local Elders Commity Name:</span>
+							{{elder.local_commity_elder_name}}
+						</p>
+						<p>
+							<span class="text-muted text-small mb-2">Local Elders Commity Membership No:</span>
+							{{elder.local_commity_elder_id}}
+						</p>
+						<p class="text-muted text-small mb-2">Nearest Post Office</p>
+						<p class="mb-3">{{elder.near_post_office_id}}</p>
+
+						<p class="text-muted text-small mb-2">Lives With Whome</p>
+						<p class="mb-3">{{elder.lives_with_whome}}</p>
+
+						<p class="text-muted text-small mb-2">Other Elder Nic</p>
+						<p class="mb-3">{{elder.other_elders_nic}}</p>
+
+						<p class="text-muted text-small mb-2">Samurdhi Number</p>
+						<p class="mb-3">{{elder.samurdi_no}}</p>
+
+						<p class="text-muted text-small mb-2">Public Aid Number</p>
+						<p class="mb-3">{{elder.people_adi_no}}</p>
 					</b-colxx>
 					<b-colxx lg="6" md="12" class="mb-4">
-						<div class="m-4">
-							<p class="text-muted text-small mb-2">Elder Full Name</p>
-							<p class="mb-3">{{elder.name}}</p>
+						<p class="text-muted text-small mb-2">Other Details</p>
+						<p class="mb-3">{{elder.other_name_and_description}}</p>
 
-							<p class="text-muted text-small mb-2">Elder Address</p>
-							<p class="mb-3">{{elder.address}}</p>
+						<p class="text-muted text-small mb-2">Source Of Income</p>
+						<p class="mb-3">{{elder.source_of_income}}</p>
 
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Sex:</span>
-								{{elder.sex}}
-							</p>
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Phone No:</span>
-								{{elder.number}}
-							</p>
-
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Nic No:</span>
-								{{elder.nic_id}}
-							</p>
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Email:</span>
-								{{elder.email}}
-							</p>
-
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Date of Birth:</span>
-								{{elder.birth_day}}
-							</p>
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Age:</span>
-								{{elder.birth_day}} ---> 97
-							</p>
-						</div>
+						<p class="text-muted text-small mb-2">Monthly Income</p>
+						<p class="mb-3">{{elder.income}}</p>
 					</b-colxx>
 				</b-row>
-				<b-card-body>
-					<b-row>
-						<b-colxx lg="6" md="12" class="mb-4">
-							<p class="mb-3">
-								<span class="text-muted text-small mb-2">Local Elders Commity Name:</span>
-								{{elder.local_commity_elder_name}}
-							</p>
-							<p>
-								<span class="text-muted text-small mb-2">Local Elders Commity Membership No:</span>
-								{{elder.local_commity_elder_id}}
-							</p>
-							<p class="text-muted text-small mb-2">Nearest Post Office</p>
-							<p class="mb-3">{{elder.near_post_office_id}}</p>
+				<b-colxx xxs="12">
+					<b-card class="mb-4" title="Officer Rewiwe About the Elder">
+						<b-form @submit.prevent="onValitadeFormSubmit" class="av-tooltip tooltip-label-right">
+							<b-form-group label="Officer Comment">
+								<b-form-textarea
+									type="text"
+									v-model="$v.div_comment.$model"
+									:state="!$v.div_comment.$error"
+								/>
 
-							<p class="text-muted text-small mb-2">Lives With Whome</p>
-							<p class="mb-3">{{elder.lives_with_whome}}</p>
+								<b-form-invalid-feedback v-if="!$v.div_comment.required">Officer Comment Is reqiured</b-form-invalid-feedback>
 
-							<p class="text-muted text-small mb-2">Other Elder Nic</p>
-							<p class="mb-3">{{elder.other_elders_nic}}</p>
-
-							<p class="text-muted text-small mb-2">Samurdhi Number</p>
-							<p class="mb-3">{{elder.samurdi_no}}</p>
-
-							<p class="text-muted text-small mb-2">Public Aid Number</p>
-							<p class="mb-3">{{elder.people_adi_no}}</p>
-						</b-colxx>
-						<b-colxx lg="6" md="12" class="mb-4">
-							<p class="text-muted text-small mb-2">Other Details</p>
-							<p class="mb-3">{{elder.other_name_and_description}}</p>
-
-							<p class="text-muted text-small mb-2">Source Of Income</p>
-							<p class="mb-3">{{elder.source_of_income}}</p>
-
-							<p class="text-muted text-small mb-2">Monthly Income</p>
-							<p class="mb-3">{{elder.income}}</p>
-						</b-colxx>
-					</b-row>
-					<b-colxx xxs="12">
-						<b-card class="mb-4" title="Officer Rewiwe About the Elder">
-							<b-form @submit.prevent="onValitadeFormSubmit" class="av-tooltip tooltip-label-right">
-								<b-form-group label="Officer Comment">
-									<b-form-textarea
-										type="text"
-										v-model="$v.div_comment.$model"
-										:state="!$v.div_comment.$error"
-									/>
-
-
-									<b-form-invalid-feedback v-if="!$v.div_comment.required">Officer Comment Is reqiured</b-form-invalid-feedback>
-
-									<b-form-invalid-feedback
-										v-else-if="!$v.div_comment.minLength || !$v.div_comment.maxLength"
-									>The Comment Should be between 10 and 256</b-form-invalid-feedback>
-								</b-form-group>
-								<b-row>
-									<b-colxx lg="6" md="12" class="mb-4 text-center">
-										<b-button type="button" variant="primary" @click.prevent="accept">Aprove</b-button>
-									</b-colxx>
-									<b-colxx lg="6" md="12" class="mb-4 text-center">
-										<b-button type="submit" variant="primary">Disqualify</b-button>
-									</b-colxx>
-								</b-row>
-							</b-form>
-						</b-card>
-					</b-colxx>
-				</b-card-body>
-			</b-card>
-		</b-colxx>
-	</AppLayout>
+								<b-form-invalid-feedback
+									v-else-if="!$v.div_comment.minLength || !$v.div_comment.maxLength"
+								>The Comment Should be between 10 and 256</b-form-invalid-feedback>
+							</b-form-group>
+							<b-row>
+								<b-colxx lg="6" md="12" class="mb-4 text-center">
+									<b-button type="button" variant="primary" @click.prevent="accept">Aprove</b-button>
+								</b-colxx>
+								<b-colxx lg="6" md="12" class="mb-4 text-center">
+									<b-button type="submit" variant="primary">Disqualify</b-button>
+								</b-colxx>
+							</b-row>
+						</b-form>
+					</b-card>
+				</b-colxx>
+			</b-card-body>
+		</b-card>
+	</b-colxx>
 </template>
 
 <script>
