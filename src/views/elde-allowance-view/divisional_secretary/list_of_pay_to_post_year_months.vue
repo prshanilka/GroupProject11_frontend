9c2c052045b264cd28e2fcbc8c41f2cb90183e78
unknown <sahanbcsrh@gmail.com>
2020-12-04 18:16:15 +0530

    pay srtucture

---
+++ src/views/elde-allowance-view/divisional_secretary/list_of_pay_to_post_year_months.vue
@@ -0,0 +1,345 @@
+ 
+ 
+ <template>
+	<AppLayout>
+		<div>
+			<datatable-heading
+				:title="title"
+				:selectAll="selectAll"
+				:isSelectedAll="isSelectedAll"
+				:isAnyItemSelected="isAnyItemSelected"
+				:keymap="keymap"
+				:changePageSize="changePageSize"
+				:searchChange="searchChange"
+				:from="from"
+				:to="to"
+				:total="total"
+				:perPage="perPage"
+			></datatable-heading>
+			<b-row>
+				<b-colxx xxs="12">
+					<vuetable
+						ref="vuetable"
+						class="table-divided order-with-arrow"
+						:http-fetch="getData"
+						:api-url="apiBase"
+						:query-params="makeQueryParams"
+						:per-page="perPage"
+						:reactive-api-url="true"
+						:fields="fields"
+						pagination-path
+						:row-class="onRowClass"
+						@vuetable:pagination-data="onPaginationData"
+						@vuetable:row-clicked="rowClicked"
+						@vuetable:cell-rightclicked="rightClicked"
+					>
+						<template slot="actions" slot-scope="props">
+							<b-button class="mb-1" @click="showList(props)" variant="outline-primary">View List</b-button>
+						</template>
+					</vuetable>
+					<vuetable-pagination-bootstrap
+						class="mt-4"
+						ref="pagination"
+						@vuetable-pagination:change-page="onChangePage"
+					/>
+				</b-colxx>
+			</b-row>
+
+			<v-contextmenu ref="contextmenu">
+				<v-contextmenu-item @click="onContextMenuAction('copy')">
+					<i class="simple-icon-docs" />
+					<span>Copy</span>
+				</v-contextmenu-item>
+				<v-contextmenu-item @click="onContextMenuAction('move-to-archive')">
+					<i class="simple-icon-drawer" />
+					<span>Move to archive</span>
+				</v-contextmenu-item>
+				<v-contextmenu-item @click="onContextMenuAction('delete')">
+					<i class="simple-icon-trash" />
+					<span>Delete</span>
+				</v-contextmenu-item>
+			</v-contextmenu>
+		</div>
+	</AppLayout>
+</template>
+
+<script>
+import axios from "axios";
+import AppLayout from "../../../layouts/EAppLayout";
+import Vuetable from "vuetable-2/src/components/Vuetable";
+import VuetablePaginationBootstrap from "../../../components/Common/VuetablePaginationBootstrap";
+import { bUrl } from "../../../constants/config";
+import DatatableHeading from "../../../containers/datatable/DatatableHeading";
+export default {
+	props: ["title"],
+	components: {
+		AppLayout: AppLayout,
+		vuetable: Vuetable,
+		"vuetable-pagination-bootstrap": VuetablePaginationBootstrap,
+		"datatable-heading": DatatableHeading
+	},
+	data() {
+		return {
+			mm: "",
+			title: "",
+			apiBase: bUrl + "/elders",
+			isLoad: false,
+			sort: "",
+			page: 1,
+			perPage: 8,
+			search: "",
+			from: 0,
+			to: 0,
+			total: 0,
+			lastPage: 0,
+			items: [],
+			selectedItems: [],
+
+			fields: [
+				{
+					name: "payment_id",
+					sortField: "payment_id",
+					title: "PAy Id",
+					titleClass: "",
+					dataClass: "list-item-heading",
+					width: "5%"
+				},
+				{
+					name: "post_office_id",
+					sortField: "post_office_id",
+					title: "Post Code",
+					titleClass: "",
+					dataClass: "list-item-heading",
+					width: "5%"
+				},
+				{
+					name: "name",
+					sortField: "name",
+					title: "Name",
+					titleClass: "",
+					dataClass: "list-item-heading",
+					width: "20%"
+				},
+				{
+					name: "address",
+					sortField: "Address",
+					title: "address",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "bank_account_no",
+					sortField: "bank_account_no",
+					title: "bank_account_no",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+
+				{
+					name: "year",
+					sortField: "year",
+					title: "Year",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "m_name",
+					sortField: "m_name",
+					title: "Month",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "total_money_amount",
+					sortField: "total_money_amount",
+					title: "Money Amount",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "15%"
+				},
+				{
+					name: "__slot:actions",
+					title: "",
+					titleClass: "center aligned text-right",
+					dataClass: "center aligned text-right",
+					width: "20%"
+				}
+			]
+		};
+	},
+	props: ["year", "month"],
+	created() {
+		axios({
+			method: "get",
+			url:
+				"http://localhost:3000/api/paymentdivoff/topostbyyearmaonth/" +
+				this.year +
+				"/" +
+				this.month
+		}).then(res => {
+			this.mm = res.data.data[0].m_name;
+			this.title =
+				"This is Divisional List Of elder Payments To the Post Officer " +
+				this.year +
+				"  " +
+				this.mm;
+		});
+	},
+	methods: {
+		showList(props) {
+			console.log(props);
+			const uu =
+				"/division/list-paybenifisher-by-pay_id/" + props.rowData.payment_id;
+
+			this.$router.push(uu);
+		},
+		getData() {
+			return axios({
+				method: "get",
+				url:
+					"http://localhost:3000/api/paymentdivoff/topostbyyearmaonth/" +
+					this.year +
+					"/" +
+					this.month
+			}).then(res => {
+				console.log(res);
+				this.mm = res.data.data[0].m_name;
+				return res;
+			});
+		},
+		makeQueryParams(sortOrder, currentPage, perPage) {
+			this.selectedItems = [];
+			return sortOrder[0]
+				? {
+						sort: sortOrder[0]
+							? sortOrder[0].field + "|" + sortOrder[0].direction
+							: "",
+						page: currentPage,
+						per_page: this.perPage,
+						search: this.search
+				  }
+				: {
+						page: currentPage,
+						per_page: this.perPage,
+						search: this.search
+				  };
+		},
+		onRowClass(dataItem, index) {
+			if (this.selectedItems.includes(dataItem.payment_id)) {
+				return "selected";
+			}
+			return "";
+		},
+
+		rowClicked(dataItem, event) {
+			const itemId = dataItem.payment_id;
+			if (event.shiftKey && this.selectedItems.length > 0) {
+				let itemsForToggle = this.items;
+				var start = this.getIndex(itemId, itemsForToggle, "payment_id");
+				var end = this.getIndex(
+					this.selectedItems[this.selectedItems.length - 1],
+					itemsForToggle,
+					"payment_id"
+				);
+				itemsForToggle = itemsForToggle.slice(
+					Math.min(start, end),
+					Math.max(start, end) + 1
+				);
+				this.selectedItems.push(
+					...itemsForToggle.map(item => {
+						return item.payment_id;
+					})
+				);
+				this.selectedItems = [...new Set(this.selectedItems)];
+			} else {
+				if (this.selectedItems.includes(itemId)) {
+					this.selectedItems = this.selectedItems.filter(x => x !== itemId);
+				} else this.selectedItems.push(itemId);
+			}
+		},
+		rightClicked(dataItem, field, event) {
+			event.preventDefault();
+			if (!this.selectedItems.includes(dataItem.payment_id)) {
+				this.selectedItems = [dataItem.payment_id];
+			}
+			this.$refs.contextmenu.show({ top: event.pageY, left: event.pageX });
+		},
+		onPaginationData(paginationData) {
+			this.from = paginationData.from;
+			this.to = paginationData.to;
+			this.total = paginationData.total;
+			this.lastPage = paginationData.last_page;
+			this.items = paginationData.data;
+			this.$refs.pagination.setPaginationData(paginationData);
+		},
+		onChangePage(page) {
+			this.$refs.vuetable.changePage(page);
+		},
+
+		changePageSize(perPage) {
+			this.perPage = perPage;
+			this.$refs.vuetable.refresh();
+		},
+
+		searchChange(val) {
+			this.search = val;
+			this.$refs.vuetable.refresh();
+		},
+
+		selectAll(isToggle) {
+			if (this.selectedItems.length >= this.items.length) {
+				if (isToggle) this.selectedItems = [];
+			} else {
+				this.selectedItems = this.items.map(x => x.payment_id);
+			}
+		},
+		keymap(event) {
+			switch (event.srcKey) {
+				case "select":
+					this.selectAll(false);
+					break;
+				case "undo":
+					this.selectedItems = [];
+					break;
+			}
+		},
+		getIndex(value, arr, prop) {
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i][prop] === value) {
+					return i;
+				}
+			}
+			return -1;
+		},
+
+		onContextMenuAction(action) {
+			console.log(
+				"context menu item clicked - " + action + ": ",
+				this.selectedItems
+			);
+		}
+	},
+	computed: {
+		isSelectedAll() {
+			return this.selectedItems.length >= this.items.length;
+		},
+		isAnyItemSelected() {
+			return (
+				this.selectedItems.length > 0 &&
+				this.selectedItems.length < this.items.length
+			);
+		}
+	}
+};
+</script>
+
+
+
+ 
+
+
+ 
---
+++ src/views/elde-allowance-view/divisional_secretary/list_paybenifisher_by_post_office.vue
@@ -0,0 +1,306 @@
+ 
+ 
+ <template>
+	<AppLayout>
+		<h1>{{this.pay_id}}</h1>
+		<div>
+			<datatable-heading
+				title="This is Divisional office List Of elder Payments To the Benifisher"
+				:selectAll="selectAll"
+				:isSelectedAll="isSelectedAll"
+				:isAnyItemSelected="isAnyItemSelected"
+				:keymap="keymap"
+				:changePageSize="changePageSize"
+				:searchChange="searchChange"
+				:from="from"
+				:to="to"
+				:total="total"
+				:perPage="perPage"
+			></datatable-heading>
+			<b-row>
+				<b-colxx xxs="12">
+					<vuetable
+						ref="vuetable"
+						class="table-divided order-with-arrow"
+						:http-fetch="getData"
+						:api-url="apiBase"
+						:query-params="makeQueryParams"
+						:per-page="perPage"
+						:reactive-api-url="true"
+						:fields="fields"
+						pagination-path
+						:row-class="onRowClass"
+						@vuetable:pagination-data="onPaginationData"
+						@vuetable:row-clicked="rowClicked"
+						@vuetable:cell-rightclicked="rightClicked"
+					></vuetable>
+					<vuetable-pagination-bootstrap
+						class="mt-4"
+						ref="pagination"
+						@vuetable-pagination:change-page="onChangePage"
+					/>
+				</b-colxx>
+			</b-row>
+
+			<v-contextmenu ref="contextmenu">
+				<v-contextmenu-item @click="onContextMenuAction('copy')">
+					<i class="simple-icon-docs" />
+					<span>Copy</span>
+				</v-contextmenu-item>
+				<v-contextmenu-item @click="onContextMenuAction('move-to-archive')">
+					<i class="simple-icon-drawer" />
+					<span>Move to archive</span>
+				</v-contextmenu-item>
+				<v-contextmenu-item @click="onContextMenuAction('delete')">
+					<i class="simple-icon-trash" />
+					<span>Delete</span>
+				</v-contextmenu-item>
+			</v-contextmenu>
+		</div>
+	</AppLayout>
+</template>
+
+<script>
+import axios from "axios";
+import AppLayout from "../../../layouts/EAppLayout";
+import Vuetable from "vuetable-2/src/components/Vuetable";
+import VuetablePaginationBootstrap from "../../../components/Common/VuetablePaginationBootstrap";
+import { bUrl } from "../../../constants/config";
+import DatatableHeading from "../../../containers/datatable/DatatableHeading";
+export default {
+	props: ["title"],
+	components: {
+		name: "post-officer-eldesr-payment",
+		AppLayout: AppLayout,
+		vuetable: Vuetable,
+		"vuetable-pagination-bootstrap": VuetablePaginationBootstrap,
+		"datatable-heading": DatatableHeading
+	},
+	data() {
+		return {
+			apiBase: bUrl + "/elders",
+			isLoad: false,
+			sort: "",
+			page: 1,
+			perPage: 8,
+			search: "",
+			from: 0,
+			to: 0,
+			total: 0,
+			lastPage: 0,
+			items: [],
+			selectedItems: [],
+
+			fields: [
+				{
+					name: "payment_id",
+					sortField: "payment_id",
+					title: "Pay Id",
+					titleClass: "",
+					dataClass: "list-item-heading",
+					width: "5%"
+				},
+
+				{
+					name: "elder_id",
+					sortField: "elder_id",
+					title: "Elder Id",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "5%"
+				},
+				{
+					name: "name",
+					sortField: "name",
+					title: "Name",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+
+				{
+					name: "nic_id",
+					sortField: "nic_id",
+					title: "Nic Id",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "number",
+					sortField: "number",
+					title: "Phone No",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "ajent_available",
+					sortField: "ajent_available",
+					title: "Gradian State",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "5%"
+				},
+				{
+					name: "money_amount",
+					sortField: "money_amount",
+					title: "Money",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "10%"
+				},
+				{
+					name: "is_taken_money",
+					sortField: "is_taken_money",
+					title: "revive",
+					titleClass: "",
+					dataClass: "text-muted",
+					width: "5%"
+				}
+			]
+		};
+	},
+	props: ["pay_id"],
+	methods: {
+		getData() {
+			return axios({
+				method: "get",
+				url: "http://localhost:3000/api/paymentposttoben/payid/" + this.pay_id
+			}).then(res => {
+				console.log(res.data.data[0]);
+				return res;
+			});
+		},
+		makeQueryParams(sortOrder, currentPage, perPage) {
+			this.selectedItems = [];
+			return sortOrder[0]
+				? {
+						sort: sortOrder[0]
+							? sortOrder[0].field + "|" + sortOrder[0].direction
+							: "",
+						page: currentPage,
+						per_page: this.perPage,
+						search: this.search
+				  }
+				: {
+						page: currentPage,
+						per_page: this.perPage,
+						search: this.search
+				  };
+		},
+		onRowClass(dataItem, index) {
+			if (this.selectedItems.includes(dataItem.id)) {
+				return "selected";
+			}
+			return "";
+		},
+
+		rowClicked(dataItem, event) {
+			const itemId = dataItem.id;
+			if (event.shiftKey && this.selectedItems.length > 0) {
+				let itemsForToggle = this.items;
+				var start = this.getIndex(itemId, itemsForToggle, "id");
+				var end = this.getIndex(
+					this.selectedItems[this.selectedItems.length - 1],
+					itemsForToggle,
+					"id"
+				);
+				itemsForToggle = itemsForToggle.slice(
+					Math.min(start, end),
+					Math.max(start, end) + 1
+				);
+				this.selectedItems.push(
+					...itemsForToggle.map(item => {
+						return item.id;
+					})
+				);
+				this.selectedItems = [...new Set(this.selectedItems)];
+			} else {
+				if (this.selectedItems.includes(itemId)) {
+					this.selectedItems = this.selectedItems.filter(x => x !== itemId);
+				} else this.selectedItems.push(itemId);
+			}
+		},
+		rightClicked(dataItem, field, event) {
+			event.preventDefault();
+			if (!this.selectedItems.includes(dataItem.id)) {
+				this.selectedItems = [dataItem.id];
+			}
+			this.$refs.contextmenu.show({ top: event.pageY, left: event.pageX });
+		},
+		onPaginationData(paginationData) {
+			this.from = paginationData.from;
+			this.to = paginationData.to;
+			this.total = paginationData.total;
+			this.lastPage = paginationData.last_page;
+			this.items = paginationData.data;
+			this.$refs.pagination.setPaginationData(paginationData);
+		},
+		onChangePage(page) {
+			this.$refs.vuetable.changePage(page);
+		},
+
+		changePageSize(perPage) {
+			this.perPage = perPage;
+			this.$refs.vuetable.refresh();
+		},
+
+		searchChange(val) {
+			this.search = val;
+			this.$refs.vuetable.refresh();
+		},
+
+		selectAll(isToggle) {
+			if (this.selectedItems.length >= this.items.length) {
+				if (isToggle) this.selectedItems = [];
+			} else {
+				this.selectedItems = this.items.map(x => x.id);
+			}
+		},
+		keymap(event) {
+			switch (event.srcKey) {
+				case "select":
+					this.selectAll(false);
+					break;
+				case "undo":
+					this.selectedItems = [];
+					break;
+			}
+		},
+		getIndex(value, arr, prop) {
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i][prop] === value) {
+					return i;
+				}
+			}
+			return -1;
+		},
+
+		onContextMenuAction(action) {
+			console.log(
+				"context menu item clicked - " + action + ": ",
+				this.selectedItems
+			);
+		}
+	},
+	computed: {
+		isSelectedAll() {
+			return this.selectedItems.length >= this.items.length;
+		},
+		isAnyItemSelected() {
+			return (
+				this.selectedItems.length > 0 &&
+				this.selectedItems.length < this.items.length
+			);
+		}
+	}
+};
+</script>
+
+
+
+ 
+
+
+ 
