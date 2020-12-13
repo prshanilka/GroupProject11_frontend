64fb6e4f3a4bea9b0b1e22eb356bfa22862f0bac
prshanilka <ramesh.shanilka@gmail.com>
2020-12-13 21:14:41 +0530

    g

---
+++ src/views/divisionaloffhead/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<app-layout>
+		<router-view />
+	</app-layout>
+</template>
+
+<script>
+
+import AppLayout from "../../layouts/EAppLayout";
+
+export default {
+	components: {
+		"app-layout": AppLayout
+	}
+};
+</script>
