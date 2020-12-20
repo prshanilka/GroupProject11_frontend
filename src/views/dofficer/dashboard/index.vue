d2067a0c862a091473ba13640ad456bb5036137f
gmgpathum@gmail.com <gmgpathum@gmail.com>
2020-12-20 13:34:00 +0530

    dofficer dashboard

--- src/views/dofficer/dashboard/index.vue
+++ src/views/dofficer/dashboard/index.vue
@@ -1,26 +1,77 @@
 <template>
-  <div>
-          <user-card-elder></user-card-elder>
-  </div>
+  <b-row>
+    <b-colxx  lg="12" xxs="12">
+      <b-card class="mb-4 text-center">
+        <h1 class="text-xlarge text-primary">{{ div_office.name }}</h1>   
+      </b-card>
+    </b-colxx>
+
+    <b-colxx   lg="4" xxs="12">
+      <b-card class="mb-4 text-center">
+    
+        <div class="icon-row-item">
+			      <i class="iconsminds-post-office text-xlarge"/>
+        </div>
+		
+            <p class="lead text-center text-success">Post Offices</p>
+            <p class="lead text-center text-danger">{{ div_office.post }}</p>  
+    </b-card>
+  </b-colxx>
+
+  <b-colxx  lg="4" xxs="12">
+      <b-card class="mb-4 text-center">
+    
+        <div class="icon-row-item">
+			      <i class="iconsminds-the-white-house text-xlarge"/>
+        </div>
+		
+            <p class="lead text-center text-success">Gramaniladari Divisions</p>
+            <p class="lead text-center text-danger">4</p> 
+    </b-card>
+  </b-colxx>
+
+  <b-colxx  lg="4" xxs="12">
+      <b-card class="mb-4 text-center">
+    
+        <div class="icon-row-item">
+			      <i class="iconsminds-male-female text-xlarge"/>
+        </div>
+            <p class="lead text-center text-success">Benifishers</p>
+            <p class="lead text-center text-danger">{{ div_office.count_of_benifishers_elders }}</p>  
+    </b-card>
+  </b-colxx>
+
+    <b-colxx md="12" sm="12" lg="12" xxs="12">
+      <user-card-elder></user-card-elder>
+    </b-colxx>
+  </b-row>
 </template>
 
+
+
+
 <script>
-import { mapGetters, mapActions } from "vuex";
-
 import UserCardElder from "../../../containers/elder-alowance/dofficer/UserCard";
+import AppLayout from "../../../layouts/EAppLayout";
+import axios from "axios";
+
 export default {
-
-  components: {
-   "user-card-elder": UserCardElder,
-  },
-  computed: {
-  
-
+	components: {
+    AppLayout: AppLayout,
+    "user-card-elder": UserCardElder,
 	},
-  methods: {
-    
+	data() {
+		return {
+			div_office: [],
+		}
+	},
+	async beforeCreate() {
+		axios
+			.get("/divisionaloffice/office")
+			.then(result => {
+				console.log(result.data.data[0]);
+        this.div_office = result.data.data[0];
+			});
   },
-  mounted() {
-  }
 };
 </script>
